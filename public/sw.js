57fdc312a7fbda79c582400e2748272be8961277
lucgus11 <lucas.gustin@indse.be>
2026-03-09 14:32:23 +0100

    Add service worker for caching assets

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'newsbase-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(res => res || fetch(event.request))));
